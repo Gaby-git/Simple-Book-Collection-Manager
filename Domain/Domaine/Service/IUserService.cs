6ad901681b918ffead37a16e39f39c4409d522ce
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-01 00:55:02 +0100

    Revert "code refactoring"
    This reverts commit 7090dabcbadec4cc576ff0785c9382c74108919b.

--- Domain/Domaine/Service/IUserService.cs
+++ Domain/Domaine/Service/IUserService.cs
@@ -13,8 +13,5 @@ namespace Domaine.Service
         public User DeleteUser(int userId);
 
         public IEnumerable<Book> GetUserBooks(int userId);
-
-        public IEnumerable<Book> et(int userId);
-
     }
 }
