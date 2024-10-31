40b94a3fbea343349076a23a7a53917dd7b99e55
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-01 00:23:10 +0100

    Added Book class and related properties

---
+++ Domain/Domaine/Model/BookStatistics.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Domaine.Model
+{
+    public class BookStatistics
+    {
+        public int TotalBooks { get; set; }
+        public int BooksRead { get; set; }
+        public int BooksUnread { get; set; }
+        public string FavoriteGenre { get; set; } // Based on the highest number of books
+    }
+
+}
---
+++ Domain/Domaine/Model/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domaine.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<Book> Books { get; set; } // Collection of books associated with the user
+    }
+
+}
