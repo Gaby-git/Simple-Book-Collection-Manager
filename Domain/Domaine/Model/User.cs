8f912fa4476fb58a0e11165c23677215a1a45441
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-04 12:21:38 +0100

    code evolution on controller, store, service

--- Domain/Domaine/Model/User.cs
+++ Domain/Domaine/Model/User.cs
@@ -3,7 +3,7 @@ namespace Domaine.Model
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
---
+++ Simple Book Collection Manager/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Domaine.Model;
+using Domaine.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_Book_Collection_Manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            var addUser = _userService.AddUser(user);
+
+            return CreatedAtAction(nameof(GetUserById), new { userId = addUser.UserId, addUser });
+        }
+
+        [HttpGet("{UserId:int}")]
+        public IActionResult GetUserById(int userId)
+        { 
+            var getById =  _userService.GetUserById(userId);
+
+            return Ok(getById);
+        }
+    }
+}
